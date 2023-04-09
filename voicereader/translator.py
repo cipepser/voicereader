abb346c0cfb196360bfc3e0b0d18e05e8fc0326d
cipepser <respepic@gmail.com>
2023-04-09 16:31:40 +0900

    fix max_tokens

--- voicereader/translator.py
+++ voicereader/translator.py
@@ -1,6 +1,10 @@
 
 import openai
 
+# 生成されるtokenとpromptのtoken数が合計したカウントがmax_tokensになる。
+# https://platform.openai.com/docs/api-reference/completions/create
+# サンプル的に日本語と英語でtoken数は3:1くらいになるので、text-davinci-002のmax_tokensは4097に対して、3072を設定
+
 def translate_text_to_japanese(text: str, api_key: str) -> str:
     openai.api_key = api_key
 
@@ -8,7 +12,7 @@ def translate_text_to_japanese(text: str, api_key: str) -> str:
         engine="text-davinci-002",
         prompt=f"Translate the following English text to Japanese: '{text}'",
         temperature=0.7,
-        max_tokens=4097,
+        max_tokens=3072,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0,
